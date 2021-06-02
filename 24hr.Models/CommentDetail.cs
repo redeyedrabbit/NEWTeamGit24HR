04e8423cd5f94375d2d205c1ab482cd7f2e6d6f6
Ben Ellis <Benjamin.Ellis7711@gmail.com>
2021-06-02 10:55:38 -0400

    Comment controller, added commentDetail

---
+++ 24hr.Models/CommentDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24hr.Models
+{
+    class CommentDetail
+    {
+        public int CommentId { get; set; }
+        public string Text { get; set; }
+        public Guid AuthorId { get; set; }
+    }
+}
---
+++ NEWTeamGit24HR/Controllers/CommentController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NEWTeamGit24HR.Controllers
+{
+    public class CommentController : ApiController
+    {
+        [Authorize]
+        public class NoteController : ApiController
+        {
+            public IHttpActionResult Get()
+            {
+                CommentService noteService = CreateNoteService();
+                var notes = noteService.GetNotes();
+                return Ok(notes);
+            }
+
+            public IHttpActionResult Post(NoteCreate note)
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                var service = CreateNoteService();
+
+                if (!service.CreateNote(note))
+                    return InternalServerError();
+
+                return Ok();
+            }
+
+            public IHttpActionResult Get(int id)
+            {
+                CommentService commentService = CreateNoteService();
+                var note = commentService.GetCommentById(id);
+                return Ok(note);
+            }
+
+            public IHttpActionResult Delete(int id)
+            {
+                var service = CreateNoteService();
+
+                if (!service.DeleteNote(id))
+                {
+                    return InternalServerError();
+                }
+
+                return Ok();
+            }
+
+            private CommentService CreateNoteService()
+            {
+                Guid userId = Guid.Parse(User.Identity.GetUserId());
+                var noteService = new CommentService(userId);
+                return noteService;
+            }
+        }
+    }
+}
