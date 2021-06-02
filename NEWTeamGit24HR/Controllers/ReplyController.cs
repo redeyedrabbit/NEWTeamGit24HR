c42a1597e745ccce2b6357f065f8549676fa2867
Tad Luedeke <tad.luedeke@gmail.com>
2021-06-02 11:09:36 -0400

    Added ReplyController, modified ReplyService

---
+++ NEWTeamGit24HR/Controllers/ReplyController.cs
@@ -0,0 +1,29 @@
+﻿using _24hr.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NEWTeamGit24HR.Controllers
+{
+    public class ReplyController : ApiController
+    {
+        [Authorize]
+
+        public IHttpActionResult Get()
+        {
+            ReplyService replyService = CreateReplyService();
+            var replies = replyService.GetReplies();
+            return Ok(replies);
+        }
+        public ReplyService CreateReplyService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var replyService = new ReplyService(userId);
+            return replyService;
+        }
+    }
+}
